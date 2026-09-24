a2657baf652ac7860237ef5ead84a82e67ff0744
arun-thiru <arunthiru9710@gmail.com>
2026-09-24 10:12:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 from google import genai
+import os
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+client = genai.Client(api_key=GEMINI_API_KEY)
 
 st.title("💪 MESA FitBuddy")
 st.subheader("AI Fitness Plan Generator")
